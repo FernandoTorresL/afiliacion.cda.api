5cad6ef12c8a56cd5092bd6cf3bf2dd63d066ef9
Fer Torres <fertorresmx@gmail.com>
2024-06-11 13:31:35 -0600

    🧐 [ADD] Add methods to routes. Testing on Postman

--- routes/routes.js
+++ routes/routes.js
@@ -3,3 +3,13 @@ const express = require('express');
 const router = express.Router()
 
 module.exports = router;
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All Solicitudes')
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
