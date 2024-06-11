788ff7b2626c76dfbb3fb0d1aef7ecc4fecacb82
Fer Torres <fertorresmx@gmail.com>
2024-06-11 13:04:23 -0600

    🗃️ [ADD] Connect to mongodb and test

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL
 
+// Connect to DB
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+// Test connection
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database connected!');
+})
+
+// Transfer the content of Express into constant app
 const app = express();
 
 app.use(express.json());
@@ -8,3 +25,4 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
+
