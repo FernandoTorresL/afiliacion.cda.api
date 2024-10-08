ac5a3bcee9642d411267f0d412295336f6694000
Fer Torres <fertorresmx@gmail.com>
2024-10-07 18:30:18 -0600

    🍱 [ADD] new endpoints IVRO/TEC and versioning v1
    [ADD] new endpoints IVRO & TEC searching for 'atendidos', 'pendientes', 'asunto'

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,7 @@ router.get('/v1/getAll', async (req, res) => {
     const solicitud = await Model.aggregate([
       {
         $project: {
-          operacion: { $toLower: "$operacion" },
+          operacion: "$operacion" ,
           atendido: 1
         }
       },
@@ -36,3 +36,130 @@ router.get('/v1/getAll', async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
+
+//IVRO y TEC
+
+//IVRO
+
+router.get('/v1/IVRO/atendidos', async (req, res) => {
+  try{
+      const solicitud = await Model.find(
+          {
+            "operacion":  "IVRO",
+            "atendido": 1
+          }
+        ).sort(
+          {
+            "asunto": 1,
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+
+router.get('/v1/IVRO/pendientes', async (req, res) => {
+  try{
+      const solicitud = await Model.find(
+          {
+            "operacion":  "IVRO",
+            "atendido": 0
+          }
+        ).sort(
+          {
+            "asunto": 1,
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+
+router.get('/v1/IVRO/asunto/:asunto', async (req, res) => {
+  try{
+      let asunto_str = req.params.asunto;
+      const solicitud = await Model.find(
+          {
+            "operacion": "IVRO",
+            "asunto": asunto_str
+          }
+        ).sort(
+          {
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+// TEC
+
+router.get('/v1/TEC/atendidos', async (req, res) => {
+  try{
+      const solicitud = await Model.find(
+          {
+            "operacion":  "TEC",
+            "atendido": 1
+          }
+        ).sort(
+          {
+            "asunto": 1,
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+
+router.get('/v1/TEC/pendientes', async (req, res) => {
+  try{
+      const solicitud = await Model.find(
+          {
+            "operacion":  "TEC",
+            "atendido": 0
+          }
+        ).sort(
+          {
+            "asunto": 1,
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+
+router.get('/v1/TEC/asunto/:asunto', async (req, res) => {
+  try{
+      let asunto_str = req.params.asunto;
+      const solicitud = await Model.find(
+          {
+            "operacion": "TEC",
+            "asunto": asunto_str
+          }
+        ).sort(
+          {
+            "fecha": 1
+          }
+        );
+      res.status(200).json(solicitud);
+    }
+    catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
