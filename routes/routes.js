15bbf54b274b47c66067ababd1da96c93c1afc4e
Fer Torres <fertorresmx@gmail.com>
2025-05-09 13:30:31 -0600

    🍱 Add new endpoint by operation
    Add endpoint for any operation, atendido[0,1]

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,7 @@ router.get('/v1/getAll', async (req, res) => {
     const solicitud = await Model.aggregate([
       {
         $project: {
-          operacion: "$operacion" ,
+          operacion: "$operacion",
           atendido: 1
         }
       },
@@ -39,46 +39,45 @@ router.get('/v1/getAll', async (req, res) => {
 })
 
 //Get all Method Without Duplicate
-router.get('np', async (req, res) => {
+router.get('/v1/getAllWD', async (req, res) => {
   try {
     const solicitud = await Model.aggregate([
-        {
-          $group: {
-            _id: "$asunto",
-            operacion: { $first: { $toLower: "$operacion" } },
-            atendido: { $first: "$atendido" }
-          }
-        },
-        {
-          $group: {
-            _id: {
-              operacion: "$operacion",
-              atendido: "$atendido"
-            },
-            count: { $sum: 1 }
-          }
-        },
-        {
-          $project: {
-            _id: 0,
-            operacion: "$_id.operacion",
-            atendido: "$_id.atendido",
-            count: 1
-          }
-        },
-        {
-          $sort: {
-            operacion: 1, // Orden ascendente por operacion
-            atendido: 1   // Orden ascendente por atendido
-          }
+      {
+        $group: {
+          _id: "$asunto",
+          operacion: { $first: { $toLower: "$operacion" } },
+          atendido: { $first: "$atendido" }
         }
-      ]);
+      },
+      {
+        $group: {
+          _id: {
+            operacion: "$operacion",
+            atendido: "$atendido"
+          },
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          operacion: "$_id.operacion",
+          atendido: "$_id.atendido",
+          count: 1
+        }
+      },
+      {
+        $sort: {
+          operacion: 1, // Orden ascendente por operacion
+          atendido: 1   // Orden ascendente por atendido
+        }
+      }
+    ]);
 
     require('log-timestamp')
     console.log("TODOS-SIN-DUPLICADOS")
     res.json(solicitud)
   }
-
   catch (error) {
     res.status(500).json({ message: error.message })
   }
@@ -86,25 +85,88 @@ router.get('np', async (req, res) => {
 
 // Get an specific record by asunto
 router.get('/v1/asunto/:asunto', async (req, res) => {
-  try{
-      let asunto_str = req.params.asunto;
-      const solicitud = await Model.find(
-          {
-            "asunto": {
-              "$regex": asunto_str,
-              "$options": "i"
-            }
-          }
-        ).sort(
-          {
-            "fecha": 1
-          }
-        );
-      require('log-timestamp')
-      console.log("One x asunto: ", asunto_str)
-      res.status(200).json(solicitud);
-    }
-    catch (error) {
+  try {
+
+    let asunto_str = req.params.asunto;
+
+    const solicitud = await Model.find(
+      {
+        "asunto": {
+          "$regex": asunto_str,
+          "$options": "i"
+        }
+      }
+    ).sort(
+      {
+        "fecha": 1
+      }
+    );
+
+    require('log-timestamp')
+    console.log("One x asunto: ", asunto_str)
+    res.status(200).json(solicitud);
+  }
+  catch (error) {
     res.status(500).json({ message: error.message });
+  }
+})
+
+// Get all by one operation, atendido = 1
+router.get('/v1/:operacion/atendidos', async (req, res) => {
+  try {
+
+    let operacion_str = req.params.operacion;
+
+    const solicitud = await Model.find(
+      {
+        "operacion": {
+          "$regex": operacion_str,
+          "$options": "i"
+        },
+        "atendido": 1
+      }
+    ).sort(
+      {
+        "asunto": 1,
+        "fecha": 1
+      }
+    );
+
+    require('log-timestamp')
+    console.log(operacion_str, "-atendidos")
+    res.status(200).json(solicitud);
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// Get all by one operation, atendido = 0
+router.get('/v1/:operacion/pendientes', async (req, res) => {
+  try {
+
+    let operacion_str = req.params.operacion;
+
+    const solicitud = await Model.find(
+      {
+        "operacion": {
+          "$regex": operacion_str,
+          "$options": "i"
+        },
+        "atendido": 0
+      }
+    ).sort(
+      {
+        "asunto": 1,
+        "fecha": 1
+      }
+    );
+
+    require('log-timestamp')
+    console.log(operacion_str, "-pendientes")
+    res.status(200).json(solicitud);
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
   }
 })
